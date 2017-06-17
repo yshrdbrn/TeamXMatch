9f11de1957f95963a2d36a98bc8fd4b6cf89e431
Yashar <yashardabiran75@gmail.com>
2017-06-17 19:42:59 -0400

    Initial class definition

---
+++ src/BackEnd/DataSet.java
@@ -0,0 +1,12 @@
+package BackEnd;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yashardabiran on 2017-06-17.
+ */
+public class DataSet {
+    private ArrayList<UserData> users;
+
+
+}
---
+++ src/BackEnd/UserVector.java
@@ -0,0 +1,10 @@
+package BackEnd;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yashardabiran on 2017-06-17.
+ */
+public class UserVector {
+    private ArrayList<Integer> vector;
+}
