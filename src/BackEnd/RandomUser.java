95e3901d6615d53cb6e7738ee7d030b4b9b14e6d
mori <mortonahmadi@hotmail.com>
2017-06-18 11:19:14 -0400

    Added some comments for class RandomUser.

--- src/BackEnd/RandomUser.java
+++ src/BackEnd/RandomUser.java
@@ -1,6 +1,9 @@
 package BackEnd;
 import java.util.Random;
 
+/**
+ * This class generates random user's variables.
+ */
 public class RandomUser {
 	private User randomUser;
 	
@@ -11,21 +14,21 @@ public class RandomUser {
 		String firstNameRand = generateString("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 10);
 		String lastNameRand = generateString("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 10);
 		int IDRand = generateInt(1, 10);
-		boolean freeRand = generateBoolean();
+
 		int howOftenUserPlaysPerWeekRand = generateInt(1,10);
 		int socialnessRand = generateInt(1,10);
 		
 		randomUser.setName(firstNameRand, lastNameRand);
 		randomUser.setID(IDRand);
-		randomUser.setFree(freeRand);
 		randomUser.setHowOftenUserPlaysPerWeek(howOftenUserPlaysPerWeekRand);
 		randomUser.setSocialness(socialnessRand);
 	}
 		/**
-		 * Since we don't have a actual case to study on we need to generate random names.
+		 * Since we don't have a actual case to study on, we need to generate random names.
 		 * Generating a random string in order to store it as either first name or last name for dataset.
 		 * @param characters is a string that we will choose characters from.
 		 * @param length is the length of the random string that generator is supposed to give us.
+         * @return a string that needed for random name.
 		 */
 	public static String generateString(String characters, int length){
 		Random rand = new Random();
@@ -35,17 +38,22 @@ public class RandomUser {
 		}
 		return new String(text);
 	}
+
+    /**
+     * Generating a random number for user's ID.
+     * @param min the lower bound of random generator.
+     * @param max the upper bound of random generator.
+     * @return an int which is needed for user's ID.
+     */
 	public static int generateInt(int min, int max){
 		Random rand = new Random();
 		int result = rand.nextInt((max-min)+1)+min;
 		return result;
 	}
-	public static boolean generateBoolean(){
-		Random rand = new Random();
-		boolean result = rand.nextBoolean();
-		return result;
-	}
 
+    /**
+     * @return a randomUser object.
+     */
 	public User getUser() {
 		return randomUser;
 	}
