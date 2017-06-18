c9b390d0c3927dba9d4eb5ecc60576c8fd58c2cf
Yashar <yashardabiran75@gmail.com>
2017-06-18 02:54:30 -0400

    Connect  UserEnd and BackEnd

--- src/UserEnd/Conversation.java
+++ src/UserEnd/Conversation.java
@@ -1,6 +1,8 @@
 package UserEnd;
+import BackEnd.Engine;
 import BackEnd.User;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,17 +14,45 @@ public class Conversation {
         User user = new User();
         Scanner input = new Scanner(System.in);
         System.out.println("What is your first name?");
-        String firstName = user.getFirstName();
+        String firstName = input.next();
         System.out.println("What is your last name?");
-        String lastName = user.getLastName();
+        String lastName = input.next();
         user.setName(firstName,lastName);
         System.out.println("How often do you play per week?");
-        int howOftenUserPlaysPerWeek = user.getHowOftenUserPlaysPerWeek();
+        int howOftenUserPlaysPerWeek = input.nextInt();
         user.setHowOftenUserPlaysPerWeek(howOftenUserPlaysPerWeek);
         System.out.println("How social are you?");
-        int socialness = user.getSocialness();
+        int socialness = input.nextInt();
         user.setSocialness(socialness);
-        
+
+        Engine.addUser(user);
+        Engine.changeUserStatus(user, false);
+
+        String inputStr;
+        while (true) {
+            System.out.println("\n\nFinding you a match...");
+            ArrayList<User> otherPlayers = Engine.findMatch(user);
+            System.out.println("Here are your teammates!");
+            for (User player : otherPlayers) {
+                System.out.println(player.getFirstName() + " " + player.getLastName());
+            }
+            System.out.println("Have fun playing!!\n");
+
+
+            System.out.println("Are you done playing?");
+            inputStr = input.next();
+            if(inputStr.equals("Yes"))
+                Engine.changeUserStatus(user, true);
+            review();
+            System.out.println("Do you want to play another match?");
+            inputStr = input.next();
+            if(inputStr.equals("No"))
+                break;
+
+        }
+    }
+
+    private void review() {
     }
 
 }
