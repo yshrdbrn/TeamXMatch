826d62f46a4732703574b6f7e05385c4a032a1c8
mori <mortonahmadi@hotmail.com>
2017-06-17 22:04:26 -0400

    Added some instance variables,getter and setter methods to class User.

--- src/BackEnd/User.java
+++ src/BackEnd/User.java
@@ -6,7 +6,11 @@ import java.util.HashMap;
  * Created by yashardabiran on 2017-06-17.
  */
 public class User {
-    private String name;
+    private String firstName;
+    private String lastName;
+    private int socialness;
+    private int nbTimes;
+    private boolean isValid;
     private UserVector vector;
     private int ID;
     private boolean isFree;
@@ -21,9 +25,9 @@ public class User {
 
     //Getters
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
 
     public int getID() {
         return ID;
@@ -32,12 +36,19 @@ public class User {
     public boolean isFree() {
         return isFree;
     }
+    public int getSocialness(){
+        return socialness;
+    }
+    public boolean isValid(){
+        if(socialness>10 || socialness<0)
+            return false;
+    }
 
     //Setters
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public void setID(int ID) {
         this.ID = ID;
@@ -45,6 +56,14 @@ public class User {
 
     public void setFree(boolean free) {
         isFree = free;
+    }
+    public void setName(String firstName,String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+    public void setNbTimes(int nbTimes ){
+        this.nbTimes = nbTimes;
     }
 
     //Methods
